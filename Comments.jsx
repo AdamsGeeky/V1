947977872d7fc0858af1a26fb0536338058b7be4
AdamsGeeky <muhdadamu0008@gmail.com>
2023-05-20 15:38:17 +0100

    contents

--- Comments.jsx
+++ Comments.jsx
@@ -1,21 +1,21 @@
-import Giscus from '@giscus/react';
-import React from 'react';
+import Giscus from "@giscus/react";
+import React from "react";
 
 export default function Comments() {
   return (
     <Giscus
-  repo="AdamsGeeky/Adams"
-  repoId="MDEwOlJlcG9zaXRvcnkzMTM1Nzc4OTY="
-  category="comments"
-  categoryId="DIC_kwDOErDRqM4CWnyx"
-  mapping="url"
-  strict="0"
-  reactionsEnabled="1"
-  emitMetadata="1"
-  inputPosition="top"
-  theme="light"
-  lang="en"
-  loading="lazy"
+      repo="AdamsGeeky/Adams"
+      repoId="MDEwOlJlcG9zaXRvcnkzMTM1Nzc4OTY="
+      category="comments"
+      categoryId="DIC_kwDOErDRqM4CWnyx"
+      mapping="url"
+      strict="0"
+      reactionsEnabled="1"
+      emitMetadata="1"
+      inputPosition="top"
+      theme="light"
+      lang="en"
+      loading="lazy"
     />
   );
 }
