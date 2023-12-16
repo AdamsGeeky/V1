f21bb89a08f486ed71816095e847d53bd35bbcee
ADAMU MUHAMMAD MUHAMMAD <muhdadamu0008@gmail.com>
2023-12-16 23:53:22 +0100

    feat: Diagram

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -5,6 +5,9 @@
 // See: https://docusaurus.io/docs/api/docusaurus-config
 
 import {themes as prismThemes} from 'prism-react-renderer';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
+
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
@@ -33,7 +36,16 @@ const config = {
     defaultLocale: 'en',
     locales: ['en'],
   },
-
+ //Diagram 
+  markdown: {
+    mermaid: true,
+  },
+  themes: ['@docusaurus/theme-mermaid'],
+  themeConfig: {
+    mermaid: {
+      theme: {light: 'neutral', dark: 'forest'},
+    },
+  },
   plugins: [
     //plugin progressive web app
     [
@@ -115,8 +127,8 @@ const config = {
         },
         docs: {
           sidebarPath: './sidebars.js',
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
+          remarkPlugins: [remarkMath],
+          rehypePlugins: [rehypeKatex],
           editUrl:
             'https://github.com/AdamsGeeky',
         },
@@ -133,6 +145,15 @@ const config = {
         },
       }),
     ],
+  ],
+  stylesheets: [
+    {
+      href: 'https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css',
+      type: 'text/css',
+      integrity:
+        'sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM',
+      crossorigin: 'anonymous',
+    },
   ],
 
   themeConfig:
